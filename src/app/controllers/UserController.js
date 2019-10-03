388ac6fcce8589fc8e42a6c9fc4398c46f644508
rafaelreisramos <rrramos@gmail.com>
2019-10-02 21:11:30 -0300

    added password encription

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,9 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    this.user = await User.create(req.body);
+    const user = await User.create(req.body);
 
-    return res.send(this.user);
+    return res.send(user);
   }
 }
 
