7de2b83ca4cda40f43462555c6eed89102cf89a5
rafaelreisramos <rrramos@gmail.com>
2019-10-10 16:47:28 -0300

    eslint fixes

--- backend/src/database/migrations/20191010180859-meetups-add-field-file.js
+++ backend/src/database/migrations/20191010180859-meetups-add-field-file.js
@@ -1,16 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      'meetups',
-      'file_id',
-      {
-        type: Sequelize.INTEGER,
-        references: { model: 'files', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
-    );
+    return queryInterface.addColumn('meetups', 'file_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
   },
 
   down: queryInterface => {
